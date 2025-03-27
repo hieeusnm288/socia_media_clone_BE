c15e99ac1633d5cc216649b8821ee23a28e99ad1
Nguyen Hieu <nguyenminhhieu28801@gmail.com>
2025-03-27 08:37:29 +0700

    CORS 3000

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 dotenv.config();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Cho phép các phương thức
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cho phép các headers
+  next();
+});
+
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Chỉ cho phép frontend này truy cập
+    credentials: true, // Bật chế độ gửi cookie & auth token
+  })
+);
+
 cloudinary.config({
   cloud_name: process.env.CLOuDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
